301e3743d67cba9fe2692138da7249f556607781
Han Jang Hyun <wkdgus1164@naver.com>
2021-05-11 18:32:54 +0900

    코드 리팩토링 - 전역 변수 사용하여 품질 개선

--- three-doors/script.js
+++ three-doors/script.js
@@ -1,11 +1,15 @@
 (function () {
   const stageElem = document.querySelector('.stage');
+  let currentItem;
 
   const doorHandler = (e) => {
     const targetElem = e.target;
 
+    if (currentItem) currentItem.classList.remove('door-opened');
+
     if (targetElem.classList.contains('door-body')) {
       targetElem.parentNode.classList.add('door-opened');
+      currentItem = targetElem.parentNode;
     }
   }
 
