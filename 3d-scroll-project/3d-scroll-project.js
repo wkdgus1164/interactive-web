39ef3fe7e85efde26d13a46e969bce0fff6f1d75
Han Jang Hyun <wkdgus116490@gmail.com>
2021-05-12 17:38:06 +0900

    window의 사이즈가 변경될 때 예외 처리. 공통적으로 적용되는 기능을 함수로 분리.

--- 3d-scroll-project/3d-scroll-project.js
+++ 3d-scroll-project/3d-scroll-project.js
@@ -6,12 +6,19 @@
   // window.innerHeight : window 객체 (브라우저 창) 의 높이를 찾을 수 있다.
 
   // body 의 높이에서 브라우저 창의 높이를 빼면 전체 스크롤할 수 있는 범위가 산출된다.
-  let maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  let maxScrollValue;
 
   const windowScrollHandler = () => {
     const zMove = pageYOffset / maxScrollValue * 980 - 490;
     $houseElement.style.transform = `translateZ(${zMove}vw)`;
   }
 
+  const windowResizeHandler = () => maxScrollValue = document.body.offsetHeight - window.innerHeight;
+
+  // window 를 스크롤할 때 이벤트
   window.addEventListener('scroll', windowScrollHandler);
+
+  // window 의 사이즈가 변경될 때 이벤트
+  window.addEventListener('resize', windowResizeHandler);
+  windowResizeHandler();
 })();
