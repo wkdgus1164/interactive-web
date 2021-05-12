c8bc344a3655fb68abf29a2cf46b7daa48581cb1
Han Jang Hyun <wkdgus116490@gmail.com>
2021-05-12 17:23:52 +0900

    3D 효과 구현

--- 3d-scroll-project/3d-scroll-project.js
+++ 3d-scroll-project/3d-scroll-project.js
@@ -1,8 +1,16 @@
 (function () {
   const $houseElement = document.querySelector('.house');
 
+  // window.pageYOffset : 페이지가 스크롤될 때 현재 Y축 위치가 최상단으로부터 몇 픽셀 떨어져 있는지 찾을 수 있다.
+  // document.body.offsetHeight : body 의 높이를 찾을 수 있다.
+  // window.innerHeight : window 객체 (브라우저 창) 의 높이를 찾을 수 있다.
+
+  // body 의 높이에서 브라우저 창의 높이를 빼면 전체 스크롤할 수 있는 범위가 산출된다.
+  let maxScrollValue = document.body.offsetHeight - window.innerHeight;
+
   const windowScrollHandler = () => {
-    console.log(window.pageYOffset);
+    const zMove = pageYOffset / maxScrollValue * 980 - 490;
+    $houseElement.style.transform = `translateZ(${zMove}vw)`;
   }
 
   window.addEventListener('scroll', windowScrollHandler);
