aaf0cf64c8547eae5e192ffdee49690b6ef897d9
Han Jang Hyun <wkdgus116490@gmail.com>
2021-05-12 18:29:10 +0900

    마우스를 움직이 때 화면이 조금씩 입체적으로 시점이 전환되는 이벤트 구현

--- 3d-scroll-project/3d-scroll-project.js
+++ 3d-scroll-project/3d-scroll-project.js
@@ -2,8 +2,10 @@
 // document.body.offsetHeight : body 의 높이를 찾을 수 있다.
 // window.innerHeight : window 객체 (브라우저 창) 의 높이를 찾을 수 있다.
 (function () {
+  const $stageElement = document.querySelector('.stage');
   const $houseElement = document.querySelector('.house');
   const $barElement = document.querySelector('.progress-bar');
+  const mousePosition = { x: 0, y: 0 };
 
   let maxScrollValue;
   let scrollPercent;
@@ -21,7 +23,14 @@
 
   const progressbarController = () => $barElement.style.width = `${scrollPercent * 100}%`;
 
+  const windowMouseMoveHandler = e => {
+    mousePosition.x = -1 + (e.clientX / window.innerWidth) * 2;
+    mousePosition.y = 1 - (e.clientY / window.innerWidth) * 2;
+    $stageElement.style.transform = `rotateX(${mousePosition.y * 5}deg) rotateY(${mousePosition.x * 5}deg)`;
+  }
+
   window.addEventListener('scroll', windowScrollHandler);
   window.addEventListener('resize', windowResizeHandler);
+  window.addEventListener('mousemove', windowMouseMoveHandler);
   windowResizeHandler();
 })();
