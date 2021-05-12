15296b3339a7204cd94c65fcbd433149674761b5
Han Jang Hyun <wkdgus116490@gmail.com>
2021-05-12 18:00:31 +0900

    상단 스크롤바 추가

--- 3d-scroll-project/3d-scroll-project.js
+++ 3d-scroll-project/3d-scroll-project.js
@@ -1,24 +1,27 @@
+// window.pageYOffset : 페이지가 스크롤될 때 현재 Y축 위치가 최상단으로부터 몇 픽셀 떨어져 있는지 찾을 수 있다.
+// document.body.offsetHeight : body 의 높이를 찾을 수 있다.
+// window.innerHeight : window 객체 (브라우저 창) 의 높이를 찾을 수 있다.
 (function () {
   const $houseElement = document.querySelector('.house');
+  const $barElement = document.querySelector('.progress-bar');
 
-  // window.pageYOffset : 페이지가 스크롤될 때 현재 Y축 위치가 최상단으로부터 몇 픽셀 떨어져 있는지 찾을 수 있다.
-  // document.body.offsetHeight : body 의 높이를 찾을 수 있다.
-  // window.innerHeight : window 객체 (브라우저 창) 의 높이를 찾을 수 있다.
+  let maxScrollValue;
+  let scrollPercent;
 
   // body 의 높이에서 브라우저 창의 높이를 빼면 전체 스크롤할 수 있는 범위가 산출된다.
-  let maxScrollValue;
-
-  const windowScrollHandler = () => {
-    const zMove = pageYOffset / maxScrollValue * 980 - 490;
-    $houseElement.style.transform = `translateZ(${zMove}vw)`;
-  }
-
   const windowResizeHandler = () => maxScrollValue = document.body.offsetHeight - window.innerHeight;
 
-  // window 를 스크롤할 때 이벤트
-  window.addEventListener('scroll', windowScrollHandler);
+  const windowScrollHandler = () => {
+    scrollPercent = pageYOffset / maxScrollValue;
+    const zMove = scrollPercent * 980 - 490;
+    $houseElement.style.transform = `translateZ(${zMove}vw)`;
 
-  // window 의 사이즈가 변경될 때 이벤트
+    progressbarController();
+  }
+
+  const progressbarController = () => $barElement.style.width = `${scrollPercent * 100}%`;
+
+  window.addEventListener('scroll', windowScrollHandler);
   window.addEventListener('resize', windowResizeHandler);
   windowResizeHandler();
 })();
